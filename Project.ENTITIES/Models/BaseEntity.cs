39972e62e462051840eb538ace98927c9cbef25e
Tali Kaan BÜYÜKTEZELLER <KaanBaran@hotmail.com>
2025-04-30 13:39:20 +0300

    UpdatedDate properties eklendi.

--- Project.ENTITIES/Models/BaseEntity.cs
+++ Project.ENTITIES/Models/BaseEntity.cs
@@ -11,5 +11,7 @@ namespace Project.ENTITIES.Models
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
 
+        public DateTime? UpdatedDate { get; set; }   
+
     }
 }
